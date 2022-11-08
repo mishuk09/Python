2ba12c0c767491f1928463d82425fe54de5abdc2
mishuk09 <mishukinfo09@gmail.com>
2022-11-08 08:25:28 -0500

    first assignmernt

---
+++ Assignment-01.py
@@ -0,0 +1,59 @@
+ 
+
+# Assignement-01
+Mahadi Hasan Mishuk
+
+#21SOECE11649
+
+#CE-D roll 54
+
+"""1- Write a Python program to print “Welcome to RK university”."""
+ 
+
+print("Welcome to RK university")
+
+
+
+
+"""2- Write a Python program to print your Full name in one line and your phone number in
+next line.
+"""
+
+print("Mahadi Hasan Mishuk")
+print(7069089661)
+
+
+
+
+"""3- Write a Python program to print “you are coding using Python” in two different lines
+using one print statement.
+Output should be:
+You are coding using
+Python
+"""
+
+print("You are coding using\n python")
+
+
+
+
+"""4- Write a Python program to print your full name in one line using two print statement, and
+print your DOB as follows: 01/01/2022."""
+ 
+print("Mahadi Hasan",end=' ')
+print("Mishuk")
+print('10/03/2000')
+
+
+
+"""5- Create a simple CV using Python."""
+
+
+
+print("     Bio-Data")
+print("Name : Mahadi Hasan Mishuk")
+print("Dob : 10 March 2000")
+print("Age : 22")
+print("Contact : 7069089661")
+print("Address : Tramba,Rk University,Rajkot,Gujrat")
+
