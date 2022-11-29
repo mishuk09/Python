eeba18f1a65db51204979d1278abea0b73811ba5
mishuk09 <mishukinfo09@gmail.com>
2022-11-29 11:07:41 -0500

    py assignment

---
+++ Assingment-09.py
@@ -0,0 +1,19 @@
+'''def name(x):
+    print (x)
+
+name("Mahadi")
+'''
+
+'''
+num = 6
+
+for i in range(1, 11):
+   print(num, 'x', i, '=', num*i)
+'''
+
+'''my_list = [12, 65, 54, 39, 102, 339, 221, 50, 70, ]
+
+result = list(filter(lambda x: (x % 13 == 0), my_list))
+
+
+print(result)'''
