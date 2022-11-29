974e658350e03bde973e8887e502b4f9dce12dbf
mishuk09 <mishukinfo09@gmail.com>
2022-11-29 11:13:06 -0500

    py

--- Assingment-09.py
+++ Assingment-09.py
@@ -17,3 +17,36 @@ result = list(filter(lambda x: (x % 13 == 0), my_list))
 
 
 print(result)'''
+
+ 
+'''import math
+
+ 
+print("The gcd of 60 and 48 is : ", end="")
+print(math.gcd(60, 48))'''
+
+
+
+'''
+def sum(a): 
+    a= a + 5
+    print(a)
+
+sum'''(7)
+
+
+'''num = 5
+
+ 
+factorial = 1
+
+ 
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
+'''
